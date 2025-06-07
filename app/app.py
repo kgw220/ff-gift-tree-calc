dcf3316f19e980714a88a98e506c8cd146a13d8f
kgw220 <kevingrahamwu@gmail.com>
2025-06-07 15:44:43 -0400

    Removed compute probabilities button, and made minor revisions for a better UI

--- app/app.py
+++ app/app.py
@@ -26,30 +26,33 @@ gift_tree_cost_convert = {
 cost_per_tree = gift_tree_cost_convert[cost_per_tree]
 
 # Mode selection on rather user wants to see probabilities or percentages
-mode = st.radio(
+mode_label = st.radio(
     "Display Mode",
-    ["Probabilities", "Percentages"],
+    ["Probability", "Percentage"],
     horizontal=True
 )
 mode_convert = {
-    "Probabilities": False,
-    "Percentages": True,
+    "Probability": False,
+    "Percentage": True,
 }   
-mode = mode_convert[mode]
+mode = mode_convert[mode_label]
 
+# Calculate and display everything with the user chosen options
+gt = gift_tree(n_trees=n_trees, cost_per_tree=cost_per_tree)
+prob_of_profit, fruits, pmf, profits = gt.compute_profit_probability()
 
-if st.button("Compute Probability"):
-    gt = gift_tree(n_trees=n_trees, cost_per_tree=cost_per_tree)
-    prob_of_profit, fruits, pmf, profits = gt.compute_profit_probability()
+# If percentages are selected, show percentage of making profit. Otherwise, show probability.
+if mode:
+    st.markdown(f"### {mode_label} of making a profit from {n_trees} tree(s): `{prob_of_profit * 100:.4f}%`")
+else:
+    st.markdown(f"### {mode_label} of making a profit from {n_trees} tree(s): `{prob_of_profit:.4f}`")
 
-    st.markdown(f"### 📊 Probability of Making a Profit: `{prob_of_profit:.4f}`")
+# Summary stats
+summary_stats = gt.get_summary_stats(profits)
+st.markdown(f"#### Min Profit: {summary_stats['min_profit']:,.0f} gold")
+st.markdown(f"#### Max Profit: {summary_stats['max_profit']:,.0f} gold")
+st.markdown(f"#### Average Profit: {summary_stats['average_profit']:,.0f} gold")
 
-    # Summary stats
-    summary_stats = gt.get_summary_stats(profits)
-    st.write(f"Min Profit: {summary_stats['min_profit']:,.0f} gold")
-    st.write(f"Max Profit: {summary_stats['max_profit']:,.0f} gold")
-    st.write(f"Average Profit: {summary_stats['average_profit']:,.0f} gold")
-
-    # Display the PMF plot
-    plot = gt.get_pmf_plot(fruits, pmf, profits, mode)
-    components.html(plot, height=600)
+# Display the PMF plot
+plot = gt.get_pmf_plot(fruits, pmf, profits, mode)
+components.html(plot, height=600)
