cf4595592f29f6f2922534e51a4125667f5ca57e
kgw220 <kevingrahamwu@gmail.com>
2025-06-06 20:40:29 -0400

    Altered how gift tree class was imported for streamlit app deployment

--- app/app.py
+++ app/app.py
@@ -2,35 +2,15 @@ import sys
 import os
 import streamlit as st
 
-# Get the absolute path to the 'calculations' folder
-current_dir = os.path.dirname("app.py")                 
-project_root = os.path.abspath(os.path.join(current_dir, ".."))
-calculations_path = os.path.join(project_root, "calculations")
-
-# Add 'calculations' to sys.path
-sys.path.append(calculations_path)
-
 # Now import the class
-from gifttree import gift_tree
-
-st.markdown("""
-<style>
-@font-face {
-  font-family: 'kalyant';
-  src: url('kalyant.otf') format('opentype');
-}
-html, body, [class*="css"] {
-  font-family: 'kalyant', sans-serif;
-}
-</style>
-""", unsafe_allow_html=True)
+from calculations.gifttree import gift_tree
 
 # Streamlit App Title
 st.title("Gift Tree Profit Probability Calculator")
 
 # User Inputs
-n_trees = st.number_input("Number of Trees Harvested:", min_value=1, value=20)
-cost_per_tree = st.number_input("Cost per Tree:", value=1.9e6, format="%.1f")
+n_trees = st.slider("Number of Gift Trees", min_value=1, max_value=20, value=20, step=1)
+cost_per_tree = st.slider("Cost of Gift Tree Seed", min_value=1_900_000, max_value=2_100_000, value=(1_900_000), step=100_000)
 
 # Calculate button
 if st.button("Compute Probability"):
@@ -41,10 +21,9 @@ if st.button("Compute Probability"):
 
     # Summary stats
     summary_stats = gt.get_summary_stats(profits)
-    st.markdown(summary_stats)
-    st.write(f"Min Profit: {summary_stats['min_profit']:,.0f}")
-    st.write(f"Max Profit: {summary_stats['max_profit']:,.0f}")
-    st.write(f"Average Profit: {summary_stats['average_profit']:,.0f}")
+    st.write(f"Min Profit: {summary_stats['min_profit']:,.0f} gold")
+    st.write(f"Max Profit: {summary_stats['max_profit']:,.0f} gold")
+    st.write(f"Average Profit: {summary_stats['average_profit']:,.0f} gold")
 
     # Display the PMF plot
     plot = gt.get_prob_plot(fruits, pmf, profits)
