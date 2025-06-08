1bfecf4075631f16bc8a68fbc5a25d4764c80938
kgw220 <kevingrahamwu@gmail.com>
2025-06-08 09:15:26 -0400

    Updated sidebar text

--- app/app.py
+++ app/app.py
@@ -53,8 +53,9 @@ else:
 st.sidebar.markdown(f"Min Profit: {gt.get_min(profits):,.0f} gold")
 st.sidebar.markdown(f"Max Profit: {gt.get_max(profits):,.0f} gold")
 st.sidebar.markdown(f"Average Profit: {gt.get_average(profits):,.0f} gold")
+st.sidebar.markdown("---")
 st.sidebar.markdown(
-    '<span style="color: #262730;">Calculator only properly renders in dark mode!</span>',
+    '<span style="color: #262730;">NOTE: Calculator only properly renders in dark mode!</span>',
     unsafe_allow_html=True
 )
 
